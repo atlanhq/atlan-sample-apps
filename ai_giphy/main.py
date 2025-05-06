41ff767fa25edf212efde1054cafa2855c05d96f
Hrushikesh Dokala <hrushikesh.dokala@atlan.com>
2025-05-06 19:04:11 +0530

    feat: workflow, and activities are defined

---
+++ ai_giphy/main.py
@@ -0,0 +1,41 @@
+import asyncio
+from typing import Any, Dict
+
+from application_sdk.application import BaseApplication
+from application_sdk.common.logger_adaptors import get_logger
+
+from activities import AIGiphyActivities
+from workflow import AIGiphyWorkflow
+
+APPLICATION_NAME = "ai_giphy"
+
+logger = get_logger(__name__)
+
+
+async def main(daemon: bool = True) -> Dict[str, Any]:
+    logger.info("Starting giphy application")
+
+    # initialize application
+    app = BaseApplication(name=APPLICATION_NAME)
+
+    # setup workflow
+    await app.setup_workflow(
+        workflow_classes=[AIGiphyWorkflow],
+        activities_class=AIGiphyActivities,
+        passthrough_modules=[
+            "requests", "urllib3", "httpx", "urllib.request", 
+            "langchain_openai", "langchain_core", "langchain", "langchainhub", 
+            "warnings", "os", "grpc"
+        ],
+    )
+
+    # start worker
+    await app.start_worker()
+    await app.setup_server(workflow_class=AIGiphyWorkflow)
+
+    # start server
+    await app.start_server()
+
+
+if __name__ == "__main__":
+    asyncio.run(main(daemon=True))
