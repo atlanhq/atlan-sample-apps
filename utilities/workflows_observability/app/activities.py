879f473b89a00ad35febc02b67ebe2ef089e7e4a
Ernest Hill <105281429+ErnestoLoma@users.noreply.github.com>
2025-09-03 21:02:58 +0300

    chore: update to use pyatlan async (#42)
    * feat: Update asset_descriptor_reminder to use async atlan client
    
    * feat: update freshness monitor to use async_client.
    
    * feat: Update workflows_observability to use pyatlan async capabilities
    
    * feat: Update freshness_monitor to atlan-application-sdk==0.1.1rc36
    
    * feat: Update asset_descriptor_reminder to atlan-application-sdk==0.1.1rc36
    
    * feat: update to use application-sdk v0.1.1rc36
    
    * fix: fix formatting

--- utilities/workflows_observability/app/activities.py
+++ utilities/workflows_observability/app/activities.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from app.helpers import save_result_locally, save_result_object_storage
 from application_sdk.activities import ActivitiesInterface
-from application_sdk.clients.atlan import get_client
+from application_sdk.clients.async_atlan import get_client
 from application_sdk.observability.logger_adaptor import get_logger
 from temporalio import activity
 
@@ -13,7 +13,7 @@ activity.logger = logger
 
 class WorkflowsObservabilityActivities(ActivitiesInterface):
     @activity.defn
-    def fetch_workflows_run(self, args: tuple[str, str, str]):
+    async def fetch_workflows_run(self, args: tuple[str, str, str]):
         """
         Fetch workflow runs from Atlan within a time range, based on a selected date and output type.
         If output_type is 'Local', the workflow run results are stored as JSON files under /tmp/workflows.
@@ -46,19 +46,21 @@ class WorkflowsObservabilityActivities(ActivitiesInterface):
             time_difference = current_datetime - input_date
             difference_in_hours = int(time_difference.total_seconds() / 3600)
 
-            client = get_client()
+            client = await get_client()
             from pyatlan.model.enums import AtlanWorkflowPhase
 
-            results = client.workflow.find_runs_by_status_and_time_range(
+            results = await client.workflow.find_runs_by_status_and_time_range(
                 status=[AtlanWorkflowPhase.SUCCESS, AtlanWorkflowPhase.FAILED],
                 started_at=f"now-{difference_in_hours}h",
             )
 
-            for result in results:
+            async for result in results:
+                logger.info(f"Saving locally to {local_directory}")
                 save_result_locally(result, local_directory)
 
             if output_type == "Object Storage":
-                save_result_object_storage("", local_directory)
+                logger.info(f"Uploading to {local_directory}")
+                await save_result_object_storage(output_prefix, local_directory)
 
         except Exception as e:
             logger.error(f"Failed to process workflows: {str(e)}", exc_info=e)
